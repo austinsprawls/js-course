9494be2341828d8d7bb58a59f19bea6dae98ff63
Austin Sprawls <austinsprawls@gmail.com>
2014-07-25 15:55:22 -0500

    added user alerts and added rest of game functionality

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var spaces = [
 var player1 = 'veggies';
 var player2 = 'junkfood';
 var currentPlayer = null;
+var gameOver = false;
 
 var setNextTurn = function () {
   if (currentPlayer === player1) {
@@ -28,11 +29,17 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[6] === spaces[4] && spaces[4] === spaces[2]
     // TODO: Check for rest of game winning cases
   )
   {
     console.log('somebody won');
     // TODO: Trigger 'game-win' event with the winning player as the event data
+    $(document).trigger('game-win', currentPlayer);
   }
 };
 
@@ -42,16 +49,25 @@ $(document).on('click', '#board .space', function (e) {
 
   // Marks the space with the current player's name
   // TODO: Don't mark it unless the space is blank
-  spaces[spaceNum] = currentPlayer;
-  // Adds a class to elem so css can take care of the visuals
-  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
-
-  checkForWinner();
-  setNextTurn();
+  if(gameOver){
+    alert('The game is over! Refresh the page to play again.');
+  }else{
+    if(spaces[spaceNum]){
+      alert('That space is already taken!');
+    }else{
+      spaces[spaceNum] = currentPlayer;
+      // Adds a class to elem so css can take care of the visuals
+      $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+      checkForWinner();
+      setNextTurn();
+    }
+  }
 });
 
 $(document).on('game-win', function (e, winner) {
   // TODO: Alert who won the game
+  alert('The winner is ' + winner + '!');
+  gameOver = true;
 });
 
 // Start the game
